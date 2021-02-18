001616ba887c29bfdcf5944a3ec957256758231a
Dan Di Spaltro <dan.dispaltro@gmail.com>
2021-02-18 12:46:57 -0800

    Fix import

--- tests/src/test/scala/cats/tagless/tests/CatsTaglessTestSuite.scala
+++ tests/src/test/scala/cats/tagless/tests/CatsTaglessTestSuite.scala
@@ -2,7 +2,6 @@ package cats.tagless.tests
 
 import cats.instances.AllInstances
 import cats.tagless.syntax.AllSyntax
-import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
